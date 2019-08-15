539522af5a8524c3a989199be017d673d6135ad9
Shawn Landden <shawn@git.icu>
2019-08-15 17:33:07 -0400

    freebl: PPC support for crypto extensions, ghash (POWER8+)
    This is the code from cryptogams, which is licensed under a BSD-3 license.

---
+++ lib/freebl/build-perlasm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#/* This Source Code Form is subject to the terms of the Mozilla Public
+# * License, v. 2.0. If a copy of the MPL was not distributed with this
+# * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+perl ghashp8-ppc.pl linux64le ppc8-ghash.S
+perl ghashp8-ppc.pl linux64 ppc8-ghash-be.S
+perl ghashp8-ppc.pl linux32 ppc8-ghash-32.S
+
+
